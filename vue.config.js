7cb657a5bf9073ac27bf11a25aa5e3c8d50d56cc
Carl Banbury <carl@banbury.ch>
2022-11-18 20:33:02 +0000

    adding public path

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
-  publicPath: process.env.NODE_ENV === 'production' ? '/element-one/' : '/',
+  publicPath: '/element-one/',
   lintOnSave: false,
   runtimeCompiler: true,
   transpileDependencies: true,
