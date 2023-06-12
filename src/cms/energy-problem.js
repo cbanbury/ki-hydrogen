3a1e9004a2ae028b5567cc650c6d766ee5ad2ac0
Carl Banbury <carl@banbury.ch>
2023-06-12 23:07:06 +0100

    dropping 3x expensive text

--- src/cms/energy-problem.js
+++ src/cms/energy-problem.js
@@ -2,6 +2,4 @@ export default {
   title: "Solving Green Hydrogen's Energy Problem",
   text: 'The world needs better solutions. <strong>Existing green hydrogen</strong> technology using renewable power to split water is <strong>too energy intensive</strong>.',
   text2: 'Energy currently makes up <strong>70%</strong> of the <strong>total cost</strong> of green hydrogen.',
-  callout: '3x',
-  callout2: 'Expensive',
 };
