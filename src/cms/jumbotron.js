537a241809e9fbadeb03e3e80d38116dd2a8cc09
Carl Banbury <carl@banbury.ch>
2023-06-26 12:13:19 +0100

    Update jumbotron.js

--- src/cms/jumbotron.js
+++ src/cms/jumbotron.js
@@ -1,8 +1,4 @@
 export default {
   title: 'ANSWERING THE CLIMATE CALL TO ACTION',
   subtitle: 'We are here for the hard problems.',
-  cta: {
-    text: 'See our pitch',
-    url: 'https://youtu.be/6MJkBJTG99o',
-  },
 };
